ca497155378277c8cc5cbbc892e69b56d04df789
Sanskar-Rijal <rijalsanskar@gmail.com>
2026-08-07 11:57:36 +0545

    using OSRM api

--- services/trip-service/internal/service/service.go
+++ services/trip-service/internal/service/service.go
@@ -2,7 +2,12 @@ package service
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
 	"ride-sharing/services/trip-service/internal/domain"
+	"ride-sharing/shared/types"
 
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
@@ -11,20 +16,45 @@ type service struct {
 	repo domain.TripRepository
 }
 
-func NewService(repo domain.TripRepository) *service{
+func NewService(repo domain.TripRepository) *service {
 	return &service{
 		repo: repo,
 	}
 }
 
-func(s *service) CreateTrip(ctx context.Context, fare *domain.RideFareModel) (*domain.TripModel, error){
-
+func (s *service) CreateTrip(ctx context.Context, fare *domain.RideFareModel) (*domain.TripModel, error) {
 	t := &domain.TripModel{
-		ID: primitive.NewObjectID(),
-		UserID: fare.UserID,
-		Status: "pending",
-		RideFare: fare ,
+		ID:       primitive.NewObjectID(),
+		UserID:   fare.UserID,
+		Status:   "pending",
+		RideFare: fare,
 	}
 
-	return s.repo.CreateTrip(ctx,t)
+	return s.repo.CreateTrip(ctx, t)
+}
+
+func GetRoute(ctx context.Context, pickup, destination *types.Coordinate) (*types.OsrmApiResponse, error) {
+	baseUrl := "http://router.project-osrm.org"
+
+	url := fmt.Sprintf("%s/route/v1/driving/%f,%f;%f,%f?overview=full&geometries=geojson",
+		baseUrl,
+		pickup.Longitude, pickup.Latitude, destination.Longitude, destination.Latitude)
+
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch route from osrm api: %v", err)
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read the response: %v", err)
+	}
+
+	var routeResponse types.OsrmApiResponse
+
+	if err := json.Unmarshal(body, &routeResponse); err != nil {
+		return nil, fmt.Errorf("failed to parse the data: %v", err)
+	}
+	return &routeResponse, nil
 }
